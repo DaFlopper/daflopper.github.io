b5464c185f992b7884afc844667f5df7ac236844
DaFlopper <131584995+DaFlopper@users.noreply.github.com>
2023-07-07 17:58:59 +0200

    Create scripts.js

---
+++ finder/scripts.js
@@ -0,0 +1,10 @@
+function findClosestRestaurant() {
+  // Retrieve the user's input location
+  var locationInput = document.getElementById("location").value;
+
+  // Add your code here to find the closest restaurant based on the locationInput
+
+  // Display the map or restaurant information
+  var mapDiv = document.getElementById("map");
+  // Your code to display the map or restaurant information goes here
+}
