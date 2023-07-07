0aedf42a161bbeb8de3159093d025cef6dd92aba
DaFlopper <131584995+DaFlopper@users.noreply.github.com>
2023-07-07 18:05:38 +0200

    Update scripts.js

--- thursdaydetector/scripts.js
+++ thursdaydetector/scripts.js
@@ -1,12 +1,32 @@
-// Check if it is Thursday
-var today = new Date();
-var dayOfWeek = today.getDay();
-var isThursday = (dayOfWeek === 4); // Thursday is represented by 4 (0 = Sunday, 1 = Monday, etc.)
+// Calculate the countdown to Thursday
+function calculateCountdown() {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var daysUntilThursday = (11 - currentDay) % 7; // Thursday is represented by 4 (0 = Sunday, 1 = Monday, etc.)
+
+  // Display countdown if it's not Thursday yet
+  var countdownElement = document.getElementById("countdown");
+  if (daysUntilThursday > 0) {
+    countdownElement.textContent = "Countdown to Thursday: " + daysUntilThursday + " days";
+  } else {
+    countdownElement.textContent = "It's Thursday today!";
+  }
+}
 
 // Display message based on the day
-var messageElement = document.getElementById("message");
-if (isThursday) {
-  messageElement.textContent = "Happy Thursday!! 🎊🎊";
-} else {
-  messageElement.textContent = "It's not Thursday yet. Hang in there!";
+function displayMessage() {
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var messageElement = document.getElementById("message");
+  if (currentDay === 4) {
+    messageElement.textContent = "Happy Thursday!! 🎊🎊";
+  } else {
+    messageElement.textContent = "It's not Thursday yet. Hang in there!";
+  }
 }
+
+// Update countdown and message on page load
+calculateCountdown();
+displayMessage();
+
