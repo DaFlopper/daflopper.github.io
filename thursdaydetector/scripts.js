05684466b6446de5eb47c93e37886ab2a8e99651
DaFlopper <131584995+DaFlopper@users.noreply.github.com>
2023-07-07 18:08:57 +0200

    Update scripts.js

--- thursdaydetector/scripts.js
+++ thursdaydetector/scripts.js
@@ -1,32 +1,23 @@
-// Calculate the countdown to Thursday
-function calculateCountdown() {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var daysUntilThursday = (11 - currentDay) % 7; // Thursday is represented by 4 (0 = Sunday, 1 = Monday, etc.)
+// Unicorn name list
+var unicornNames = [
+  "Sparkle",
+  "Rainbow",
+  "Sugarplum",
+  "Glitter",
+  "Moonbeam",
+  "Stardust",
+  "Buttercup",
+  "Blossom",
+  "Crystal",
+  "Twinkle",
+  "Frosting",
+  "Enchanted"
+];
 
-  // Display countdown if it's not Thursday yet
-  var countdownElement = document.getElementById("countdown");
-  if (daysUntilThursday > 0) {
-    countdownElement.textContent = "Countdown to Thursday: " + daysUntilThursday + " days";
-  } else {
-    countdownElement.textContent = "It's Thursday today!";
-  }
+// Generate and display a random unicorn name
+function generateName() {
+  var randomIndex = Math.floor(Math.random() * unicornNames.length);
+  var randomName = unicornNames[randomIndex];
+  var unicornNameElement = document.getElementById("unicorn-name");
+  unicornNameElement.textContent = "Your Unicorn Name: " + randomName;
 }
-
-// Display message based on the day
-function displayMessage() {
-  var today = new Date();
-  var currentDay = today.getDay();
-
-  var messageElement = document.getElementById("message");
-  if (currentDay === 4) {
-    messageElement.textContent = "Happy Thursday!! 🎊🎊";
-  } else {
-    messageElement.textContent = "It's not Thursday yet. Hang in there!";
-  }
-}
-
-// Update countdown and message on page load
-calculateCountdown();
-displayMessage();
-
